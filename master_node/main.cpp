7861430852e4051f3c2b46675518f12b5589dacd
xcodeassociated <janusz.majchrzak@student.uj.edu.pl>
2016-12-18 19:58:32 +0100

    Added blank Clion projects for master and slave nodes.

---
+++ master_node/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! Master Node" << std::endl;
+    return 0;
+}
---
+++ slave_node/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! Slave node" << std::endl;
+    return 0;
+}
