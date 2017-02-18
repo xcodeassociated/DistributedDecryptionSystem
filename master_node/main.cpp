4043063f8da63cdd8abe8b2bb662ad099c77f38f
xcodeassociated <janusz.majchrzak@student.uj.edu.pl>
2017-02-18 02:57:03 +0100

    Application architecture changed - there will be one application for master and slave nodes (they will be differ based on mpi rank)

--- master_node/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World! Master Node" << std::endl;
-    return 0;
-}
--- slave_node/cxx_elements/core_application/model/Model.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by jm on 29.12.16.
-//
-
-#include "Model.hpp"
-
-namespace core {
-    
-}
